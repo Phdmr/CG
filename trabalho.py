73b88bf011beec3b463a30eab008671107775033
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-02-25 22:32:56 -0300

    Bezier

--- trabalho.py
+++ trabalho.py
@@ -1,4 +1,6 @@
 import pygame
+import numpy as np
+
 
 pygame.init()
 b = pygame.Color("black")
@@ -6,6 +8,7 @@ w = pygame.Color("white")
 r = pygame.Color("red")
 altura = 20
 largura = 20
+bufferPontos = []
 
 
 # Cria tabuleiro
@@ -212,35 +215,64 @@ def recortePoligono(data):
     return data
 
 
+# Inicio Bezier
+# find the a & b points
+def get_bezier_coef(points):
+    # since the formulas work given that we have n+1 points
+    # then n must be this:
+    n = len(points) - 1
+
+    # build coefficents matrix
+    C = 4 * np.identity(n)
+    np.fill_diagonal(C[1:], 1)
+    np.fill_diagonal(C[:, 1:], 1)
+    C[0, 0] = 2
+    C[n - 1, n - 1] = 7
+    C[n - 1, n - 2] = 2
+
+    # build points vector
+    P = [2 * (2 * points[i] + points[i + 1]) for i in range(n)]
+    P[0] = points[0] + 2 * points[1]
+    P[n - 1] = 8 * points[n - 1] + points[n]
+
+    # solve system, find a & b
+    A = np.linalg.solve(C, P)
+    B = [0] * n
+    for i in range(n - 1):
+        B[i] = 2 * points[i + 1] - A[i + 1]
+    B[n - 1] = (A[n - 1] + points[n]) / 2
+
+    return A, B
+
+# returns the general Bezier cubic formula given 4 control points
+def get_cubic(a, b, c, d):
+    return lambda t: np.power(1 - t, 3) * a + 3 * np.power(1 - t, 2) * t * b + 3 * (1 - t) * np.power(t, 2) * c + np.power(t, 3) * d
+
+# return one cubic curve for each consecutive points
+def get_bezier_cubic(points):
+    A, B = get_bezier_coef(points)
+    return [
+        get_cubic(points[i], A[i], B[i], points[i + 1])
+        for i in range(len(points) - 1)
+    ]
+
+# evalute each cubic curve on the range [0, 1] sliced in n points
+def evaluate_bezier(points, n):
+    curves = get_bezier_cubic(points)
+    return np.array([fun(t) for fun in curves for t in np.linspace(0, 1, n)])
+# Fim Bezier
+
+
 def bezier(data):
-    p1 = perguntarPonto('p1')
-    p2 = perguntarPonto('p2')
-    bufferPontos = []
+    points = np.random.rand(2, 2)
+    path = evaluate_bezier(points, 50)
+    px, py = path[:,0], path[:,1]
     p = {'x': 0, 'y': 0}
-
-    for t in range(0, 1, 0.001):
-        p = pontoCast(points.Count-1, 0, t)
-        bufferPontos.append(p)
-
-    for i in bufferPontos:
-        if i == p1 or i == p2:
-            cor = r
-        else:
-            cor = b
-        data = desenhaPonto(data, i, cor)
-
+    for i in range(len(px)):
+        p['x'] = int(px[i] * 10)
+        p['y'] = int(py[i] * 10)
+        data = desenhaPonto(data, p, b)
     return data
-
-
-def pontoCast(r, i, t):
-    if r == 0:
-        return bufferPontos[i]
-    p1 = pontoCast(r-1, i, t)
-    p2 = pontoCast(r-1, i + 1, t)
-    x = (1 - t) * p1['x'] + t * p2['x']
-    y = (1 - t) * p1['y'] + t * p2['y']
-    p = {'x': x, 'y': y}
-    return p
 
 
 def main():
